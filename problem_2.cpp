252c21a1aca91634073797c3b48972a9977b9e76
a-ganiev <129171338+a-ganiev@users.noreply.github.com>
2023-05-24 20:12:52 +0500

    Create problem_2.cpp

---
+++ problem_2.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int n, vector<vector<int> >& edges, int distanceThreshold) {
+    
+  //complete the function
+   
+}
+
+//do not modify the below code
+int main()
+{
+    int n, m, distanceThreshold;
+    cin >> n >> m >> distanceThreshold;
+    vector<vector<int> > edges;
+    for(int i = 1; i <= m; i ++)
+    {
+        int x, y, weight;
+        cin >> x >> y >> weight;
+        vector<int> f;
+        f.push_back(x);
+        f.push_back(y);
+        f.push_back(weight);
+        edges.push_back(f);
+    }
+
+    cout << solve(n, edges, distanceThreshold) << '\n';
+}
