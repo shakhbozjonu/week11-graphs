835be5779b880d5259b1ef41b4b3b81d65e5483c
a-ganiev <129171338+a-ganiev@users.noreply.github.com>
2023-05-24 20:28:05 +0500

    Create problem_1.cpp

---
+++ problem_1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<vector<int> > solve(vector<vector<int> >& image, int sr, int sc, int color) {
+
+    //completer the function
+    int n = image.size();
+}
+
+//do not modify the code below
+int main()
+{
+    int n;
+    cin >> n;
+    vector<vector<int> > image(n, vector<int>(n, 0));
+    for(int i = 0; i < n; i ++)
+        for(int j = 0; j < n; j ++)
+            cin >> image[i][j];
+    int sr, sc, color;
+    cin >> sr >> sc >> color;
+
+    vector<vector<int> > ans = solve(image, sr, sc, color);
+    for(int i = 0; i < n; i ++, cout << '\n')
+        for(int j = 0; j < n; j ++)
+            cout << ans[i][j] << ' ';
+}
