c24eec7acd8c7277dc2eb0d5c1cc66defeddd68d
shakhbozjonu <115206579+shakhbozjonu@users.noreply.github.com>
2023-05-31 23:14:25 +0500

    Update problem_1.cpp

--- problem_1.cpp
+++ problem_1.cpp
@@ -2,29 +2,45 @@
 
 using namespace std;
 
-vector<vector<int> > solve(vector<vector<int> >& image, int sr, int sc, int color) {
-
-    //completer the function
+void floodFill(vector<vector<int>>& image, int sr, int sc, int newColor, int oldColor) {
     int n = image.size();
+    int m = image[0].size();
+    
+    if (sr < 0 || sr >= n || sc < 0 || sc >= m || image[sr][sc] != oldColor || image[sr][sc] == newColor) {
+        return;
+    }
+    
+    image[sr][sc] = newColor;
+    
+    // Recursively fill neighboring pixels
+    floodFill(image, sr + 1, sc, newColor, oldColor);  // down
+    floodFill(image, sr - 1, sc, newColor, oldColor);  // up
+    floodFill(image, sr, sc + 1, newColor, oldColor);  // right
+    floodFill(image, sr, sc - 1, newColor, oldColor);  // left
 }
 
-//do not modify the code below
-int main()
-{
+vector<vector<int>> solve(vector<vector<int>>& image, int sr, int sc, int color) {
+    int oldColor = image[sr][sc];
+    floodFill(image, sr, sc, color, oldColor);
+    return image;
+}
+
+// Do not modify the code below
+int main() {
     int n;
     cin >> n;
-    vector<vector<int> > image(n, vector<int>(n, 0));
-    for(int i = 0; i < n; i ++)
-        for(int j = 0; j < n; j ++)
+    vector<vector<int>> image(n, vector<int>(n, 0));
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
             cin >> image[i][j];
+        }
+    }
     int sr, sc, color;
     cin >> sr >> sc >> color;
 
-    vector<vector<int> > ans = solve(image, sr, sc, color);
-    for(int i = 0; i < n; i ++)
-    {
-        for(int j = 0; j < n; j ++)
-        {   
+    vector<vector<int>> ans = solve(image, sr, sc, color);
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {   
             cout << ans[i][j];
             if(j != n - 1)
                 cout << ' ';
